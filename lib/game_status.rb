fe68b6d83b83927e5d26dafde1b2202d9f9c36ee
JINOOK JUNG <jinook929@gmail.com>
2020-11-12 03:49:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,36 +15,17 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def won?(board)
   counter = 1
   WIN_COMBINATIONS.each do |win_combination|
-  # [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]].each do |win_combination|
-    # counterX = 0 
-    # counterY = 0
-    # win_combination.each { |combo|
-    #   if(board[combo] == "X")
-    #     counterX += 1 
-    #   end 
-    # } 
-    # win_combination.each { |combo|
-    #   if(board[combo] == "Y")
-    #     counterY += 1 
-    #   end 
-    # } 
-    # if(counterX == 3 || counterY == 3)
-    #   return win_combination 
-    # else 
-    #   return false 
-    # end 
-    
     puts counter 
     counter += 1
     
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
+    puts "#{win_index_1} #{win_index_2} #{win_index_3}"
     
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
