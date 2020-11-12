a452091a82059421389c550e04148fc4820ca4b4
JINOOK JUNG <jinook929@gmail.com>
2020-11-12 03:30:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,8 @@ WIN_COMBINATIONS = [
 # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  # WIN_COMBINATIONS.each do |win_combination|
+  [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]].each do |win_combination|
     # counterX = 0 
     # counterY = 0
     # win_combination.each { |combo|
