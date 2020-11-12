0a6dc4541370c67e87a98b272811817253f58b1a
JINOOK JUNG <jinook929@gmail.com>
2020-11-12 03:54:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ def won?(board)
     puts "#{position_1} #{position_2} #{position_3}"
 
     
-    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "X" && position_2 == "X" && position_3 == "X"))
+    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
       return win_combination
     end
   end
