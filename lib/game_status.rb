b89be424b3f558c2183202a82f81cddf0a7dcffe
JINOOK JUNG <jinook929@gmail.com>
2020-11-12 04:17:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,14 +21,11 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    # puts "#{win_index_1} #{win_index_2} #{win_index_3}"
-    
+
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    # puts "#{position_1} #{position_2} #{position_3}"
 
-    
     if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
       return win_combination
     end
@@ -66,11 +63,8 @@ end
 
 def winner(board)
   if(!won?(board))
-    puts "no winner..."
     return nil
   else
-    puts "winner"
-    puts board[won?(board)[0]]
     if(board[won?(board)[0]] == "X")
       return "X"
     else
