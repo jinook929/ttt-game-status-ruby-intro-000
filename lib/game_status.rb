323886e6befae0a8188d9330eb46d89b66c06e49
JINOOK JUNG <jinook929@gmail.com>
2020-11-12 02:29:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,15 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(arg)
-  WIN_COMBINATIONS.each
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if(position_1 == "X" && )
+  end
 end
